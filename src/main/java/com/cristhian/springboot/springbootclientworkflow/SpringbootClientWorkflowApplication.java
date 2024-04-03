704f98d74a349df559b436c08c18f8839279f24b
Cristhian Contreras <cristhian_scb@hotmail.com>
2024-04-03 13:14:07 -0500

    chore(config): anexo configuracion inicial

---
+++ src/main/java/com/cristhian/springboot/springbootclientworkflow/SpringbootClientWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.cristhian.springboot.springbootclientworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootClientWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootClientWorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristhian/springboot/springbootclientworkflow/SpringbootClientWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristhian.springboot.springbootclientworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootClientWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
