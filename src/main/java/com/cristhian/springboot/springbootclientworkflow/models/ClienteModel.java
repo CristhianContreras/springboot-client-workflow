90a3df485d3241912a6aa24d9c619380dd98ed02
Cristhian Contreras <cristhian_scb@hotmail.com>
2024-04-04 18:41:50 -0500

    feat(models): anexo modelos base para cliente y persona

---
+++ src/main/java/com/cristhian/springboot/springbootclientworkflow/models/ClienteModel.java
@@ -0,0 +1,10 @@
+package com.cristhian.springboot.springbootclientworkflow.models;
+
+public class ClienteModel extends PersonaModel {
+
+    private Integer clienteId;
+    private String contrasena;
+    private boolean estado;
+
+    
+}
---
+++ src/main/java/com/cristhian/springboot/springbootclientworkflow/models/PersonaModel.java
@@ -0,0 +1,81 @@
+package com.cristhian.springboot.springbootclientworkflow.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "persona")
+public class PersonaModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    private String direccion;
+    private int edad;
+    private String identificacion;
+    private String genero;
+    private String nombre;
+    private Integer telefono;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+}
